dc512a8ad4719241a36b2cba2b68991fcdf4ddb1
polinakhorkova <polinakhorkova@mail.ru>
2023-10-05 10:38:15 +0500

    1

---
+++ bin/proj6.dart
@@ -0,0 +1,5 @@
+import 'package:proj6/proj6.dart' as proj6;
+
+void main(List<String> arguments) {
+  print('Hello world: ${proj6.calculate()}!');
+}
---
+++ test/proj6_test.dart
@@ -0,0 +1,8 @@
+import 'package:proj6/proj6.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
